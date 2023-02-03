dda339d8077f7a544b9e18401c6d496917c27c46
Leif <56021265+etherleif@users.noreply.github.com>
2023-02-03 01:53:08 +0100

    adding wrapped native contract for Wrapped ZEN and adding compiler config for that contract

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Marketplace Contracts based on Seaport and Reservoir 
 
 - Seaport 1.1 Release: https://github.com/ProjectOpenSea/seaport/tree/1.1
+- WZEN: taken from WETH contract on ethereum https://etherscan.io/token/0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2#code
--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,6 +15,15 @@ const config: HardhatUserConfig = {
           },
         },
       },
+      {
+        version: "0.4.22",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 1000,
+          },
+        },
+      },
     ],
     overrides: {
       "contracts/conduit/Conduit.sol": {
