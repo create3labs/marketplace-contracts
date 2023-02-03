11dd4eaa1eb2409880484ebdceca0a7171d6e82a
Leif <56021265+etherleif@users.noreply.github.com>
2023-02-03 01:52:31 +0100

    initializing hardhat and adding Seaport 1.1 contracts

---
+++ README.md
@@ -0,0 +1,3 @@
+# Marketplace Contracts based on Seaport and Reservoir 
+
+- Seaport 1.1 Release: https://github.com/ProjectOpenSea/seaport/tree/1.1
---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: {
+    // from https://github.com/ProjectOpenSea/seaport/blob/1.1/hardhat.config.ts
+    compilers: [
+      {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 19066,
+          },
+        },
+      },
+    ],
+    overrides: {
+      "contracts/conduit/Conduit.sol": {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+      "contracts/conduit/ConduitController.sol": {
+        version: "0.8.14",
+        settings: {
+          viaIR: true,
+          optimizer: {
+            enabled: true,
+            runs: 1000000,
+          },
+        },
+      },
+    },
+  },
+};
+
+export default config;
